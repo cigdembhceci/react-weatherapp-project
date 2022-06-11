fb70acd49be9e284d5edaa0b87da6748c0deab37
cigdembhceci <cigdembhceci@hotmail.com>
2022-06-11 18:19:27 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Hello</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        Hello
+        <footer>
+          <a
+            href="https://github.com/cigdembhceci/react-weatherapp-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source
+          </a>{" "}
+          coded by Cigdem B
+        </footer>
+      </div>
+    </div>
+  );
 }
